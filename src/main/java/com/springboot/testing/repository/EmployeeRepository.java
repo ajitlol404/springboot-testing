e1c140b25db110bfd1a8d7c49ade30694e43de89
AKN <tarunmicromax12@gmail.com>
2022-01-26 16:34:20 +0530

    Added some tests

--- src/main/java/com/springboot/testing/repository/EmployeeRepository.java
+++ src/main/java/com/springboot/testing/repository/EmployeeRepository.java
@@ -2,6 +2,14 @@ package com.springboot.testing.repository;
 
 import com.springboot.testing.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+
+    @Query("select e from Employee e where e.firstName=?1 and e.lastName=?2")//For JPQL query we use class name as table.
+    Employee findByJPQL(String firstName,String lastName);
 }
